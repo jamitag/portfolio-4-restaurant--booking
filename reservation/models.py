6338de8ad7797178b329bbbb0e7d0d5dd694b50f
jamitag <jamgra12@gmail.com>
2022-04-12 17:31:23 +0000

    Create reservation & contact models and html templates

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
+import uuid
+
+class reservation():
+    def __init__(self, date, parties, contact):
+        self.id = uuid.uuid4()
+        self.date = date
+        self.parties = parties
+        self.contact = contact
+
+
+class contact():
+    def __init__(self, first_name, surname, email, phone_number):
+        self.id = uuid.uuid4()
+        self.first_name = first_name
+        self.surname = surname
+        self.email = email
+        self.phone_number = phone_number
+
+
 # Create your models here.
