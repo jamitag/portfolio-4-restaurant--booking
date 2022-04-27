efb96d02a6c64d0b3bbdf7c754d7467ca381a94d
jamitag <jamgra12@gmail.com>
2022-04-27 17:47:40 +0000

    include uuid functionality to reservation id and contact id

--- reservation/models.py
+++ reservation/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+import uuid
 
 
 class contact(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField()
@@ -10,7 +11,7 @@ class contact(models.Model):
 
 
 class reservation(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     date = models.DateTimeField()
     parties = models.IntegerField()
     contact = models.ForeignKey(contact, on_delete=models.CASCADE)
