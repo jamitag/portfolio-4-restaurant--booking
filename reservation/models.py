dac56e66156d68cce8014347312dd12dd853cc30
jamitag <jamgra12@gmail.com>
2022-05-04 16:46:17 +0000

    Fixed column reference bug

--- reservation/models.py
+++ reservation/models.py
@@ -3,13 +3,17 @@ import uuid
 from django.contrib.auth.models import User
 
 
+def get_user():
+    return User.objects.all()[0]
+
+
 class contact(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     first_name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     email = models.EmailField()
     phone_number = models.CharField(max_length=11)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=get_user().pk)
 
 
 class reservation(models.Model):
